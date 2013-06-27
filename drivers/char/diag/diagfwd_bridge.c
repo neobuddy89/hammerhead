253df69d9942472dadfbb78933b3cb842bb015f6
Ravi Aravamudhan <aravamud@codeaurora.org>
2013-06-27 10:26:34 -0700

    diag: Ignore connecting to invalid hsic channel
    Diag currently has support for 4 hsic channels. Added checks to
    discard connections to invalid hsic channel in diag driver.
    
    Signed-off-by: Ravi Aravamudhan <aravamud@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/char/diag/diagfwd_bridge.c
+++ drivers/char/diag/diagfwd_bridge.c
@@ -35,7 +35,7 @@ struct diag_bridge_dev *diag_bridge;
 /* diagfwd_connect_bridge is called when the USB mdm channel is connected */
 int diagfwd_connect_bridge(int process_cable)
 {
-	int i;
+	uint8_t i;
 
 	pr_debug("diag: in %s\n", __func__);
 
@@ -45,7 +45,7 @@ int diagfwd_connect_bridge(int process_cable)
 	return 0;
 }
 
-void connect_bridge(int process_cable, int index)
+void connect_bridge(int process_cable, uint8_t index)
 {
 	int err;
 
@@ -67,6 +67,12 @@ void connect_bridge(int process_cable, int index)
 			diagfwd_connect_smux();
 		}
 	} else {
+		if (index >= MAX_HSIC_CH) {
+			pr_err("diag: Invalid hsic channel index %d in %s\n",
+							index, __func__);
+			mutex_unlock(&diag_bridge[index].bridge_mutex);
+			return;
+		}
 		if (diag_hsic[index].hsic_device_enabled &&
 			(driver->logging_mode != MEMORY_DEVICE_MODE ||
 			diag_hsic[index].hsic_data_requested)) {
@@ -76,7 +82,7 @@ void connect_bridge(int process_cable, int index)
 			 * device is not open */
 			if (!diag_hsic[index].hsic_device_opened) {
 				hsic_diag_bridge_ops[index].ctxt =
-							(void *)(index);
+							(void *)(int)(index);
 				err = diag_bridge_open(index,
 						 &hsic_diag_bridge_ops[index]);
 				if (err) {
