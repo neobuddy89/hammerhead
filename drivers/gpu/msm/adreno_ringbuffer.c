3eb8026f7f3cd743b38cdfe2cbf8fe85ff0a0553
Jordan Crouse <jcrouse@codeaurora.org>
2014-02-18 13:14:37 -0700

    msm: kgsl: Ensure that power constraints run for a full interval
    Make sure that a power constraint is given a full DCVS interval to
    influence the GPU before having it adjusted back down.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/adreno_ringbuffer.c
+++ drivers/gpu/msm/adreno_ringbuffer.c
@@ -1187,6 +1187,8 @@ void adreno_ringbuffer_set_constraint(struct kgsl_device *device,
 					pwrlevel.level = constraint;
 		}
 
+		device->pwrctrl.constraint.expires = jiffies +
+			device->pwrctrl.interval_timeout;
 	}
 
 }
