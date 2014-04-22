f47c13fbb01b2f0ee475261cf7576dd5e301bfbc
neobuddy89 <neobuddy89@gmail.com>
2014-04-23 00:44:09 +0530

    build: Use more generic ramdisk path
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- env_setup.sh
+++ env_setup.sh
@@ -20,7 +20,7 @@ else
 fi;
 
 export PARENT_DIR=`readlink -f ${KERNELDIR}/..`;
-export INITRAMFS_SOURCE=`readlink -f ${KERNELDIR}/../hammerhead_initramfs_cm`;
+export INITRAMFS_SOURCE=`readlink -f ${KERNELDIR}/../hammerhead_initramfs_caf`;
 export INITRAMFS_TMP=${KERNELDIR}/tmp/initramfs_source;
 
 # check if parallel installed, if not install
