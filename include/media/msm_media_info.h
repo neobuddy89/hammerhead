bb5814a85c356fc347fbcd896b462af71eb121a8
Pranav Vashi <neobuddy89@gmail.com>
2014-05-24 16:44:48 +0530

    msm: vidc: Fix video recording
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/media/msm_media_info.h
+++ include/media/msm_media_info.h
@@ -88,7 +88,8 @@ static inline unsigned int VENUS_EXTRADATA_SIZE(int width, int height)
 	 * In the future, calculate the size based on the w/h but just
 	 * hardcode it for now since 8K satisfies all current usecases.
 	 */
-	return 8 * 1024;
+	//return 8 * 1024;
+	return 0;  // TODO: Fix extrasize implementation. 8*1024 breaks video recording.
 }
 
 static inline unsigned int VENUS_Y_STRIDE(int color_fmt, int width)
