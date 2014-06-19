19a536949ee8a9ed91414b0e0986efe2876e1a3b
myfluxi <linflux@arcor.de>
2014-06-19 12:47:29 +0200

    msm: mdss: Turn footswitch on when stopping overlay
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2946,6 +2946,16 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 	need_cleanup = !list_empty(&mdp5_data->pipes_cleanup);
 	mutex_unlock(&mdp5_data->list_lock);
 
+	if (mdp5_data->mdata->ulps) {
+		rc = mdss_mdp_footswitch_ctrl_ulps(1, &mfd->pdev->dev);
+		if (rc) {
+			pr_err("footswitch control power on failed rc=%d\n",
+									rc);
+			return rc;
+		}
+		mdss_mdp_ctl_restore(mdp5_data->ctl);
+	}
+
 	if (need_cleanup) {
 		pr_debug("cleaning up pipes on fb%d\n", mfd->index);
 		mdss_mdp_overlay_kickoff(mfd, NULL);
