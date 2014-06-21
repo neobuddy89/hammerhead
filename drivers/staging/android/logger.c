8b2eca41f80808b0a3085815cd7118b206e4dd0f
Pranav Vashi <neobuddy89@gmail.com>
2014-06-21 23:35:10 +0530

    staging: android: logger: Disable by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -42,7 +42,7 @@
  * 1 - Auto Suspend
  * 2 - Disabled
  */
-static unsigned int log_mode = 1;
+static unsigned int log_mode = 2;
 static unsigned int log_enabled = 1; // Do not change this value
 
 module_param(log_mode, uint, S_IWUSR | S_IRUGO);
