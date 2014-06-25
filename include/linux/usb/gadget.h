52e668fae1cb0cc4b9304a311923107343f0ea2b
Manu Gautam <mgautam@codeaurora.org>
2014-06-25 12:12:53 +0530

    USB: ci13xxx_udc: Add support for UI interrupts counting
    Add counting of UI or USB transfers completion interrupts
    so that it can be used to determine USB transfers or
    BUS usage. This is a simple approach which gives
    reasonable accuracy to avoid counting actual bytes
    that got transferred over USB.
    
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/usb/gadget.h
+++ include/linux/usb/gadget.h
@@ -509,6 +509,7 @@ struct usb_gadget_ops {
  * @usb_core_id: Identifies the usb core controlled by this usb_gadget.
  *		 Used in case of more then one core operates concurrently.
  * @streaming_enabled: Enable streaming mode with usb core.
+ * @xfer_isr_count: UI (transfer complete) interrupts count
  *
  * Gadgets have a mostly-portable "gadget driver" implementing device
  * functions, handling all usb configurations and interfaces.  Gadget
@@ -547,6 +548,7 @@ struct usb_gadget {
 	struct device			dev;
 	u8				usb_core_id;
 	bool				streaming_enabled;
+	u32				xfer_isr_count;
 };
 
 static inline void set_gadget_data(struct usb_gadget *gadget, void *data)
