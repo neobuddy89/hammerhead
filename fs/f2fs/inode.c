9d1024f5775a4ef4aec4f70fbbd0e6b44ca5134d
Pranav Vashi <neobuddy89@gmail.com>
2014-07-05 11:08:24 +0530

    fs: f2fs: Fix inode compatibility
    * Needed after : https://github.com/neobuddy89/hammerhead_kernel_caf/commit/2a9cc0bf3cb72e43d144123ee30a0f8fc06cb8e1
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -293,6 +293,6 @@ void f2fs_evict_inode(struct inode *inode)
 	f2fs_unlock_op(sbi);
 
 no_delete:
-	end_writeback(inode);
+	clear_inode(inode);
 	invalidate_mapping_pages(NODE_MAPPING(sbi), inode->i_ino, inode->i_ino);
 }
