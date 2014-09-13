9758931be89e459e8cbbf2841771f3060a130196
Pranav Vashi <neobuddy89@gmail.com>
2014-09-13 13:57:28 +0530

    hammerhead: Fast charge adaptation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/dwc3/dwc3_otg.c
+++ drivers/usb/dwc3/dwc3_otg.c
@@ -24,6 +24,10 @@
 #include "io.h"
 #include "xhci.h"
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 static void dwc3_otg_reset(struct dwc3_otg *dotg);
 
 static void dwc3_otg_notify_host_mode(struct usb_otg *otg, int host_mode);
@@ -519,7 +523,7 @@ static int dwc3_otg_set_power(struct usb_phy *phy, unsigned mA)
 
 	ac_psy = power_supply_get_by_name("ac");
 
-	if (dotg->charger->chg_type == DWC3_DCP_CHARGER && ac_psy) {
+	if ((dotg->charger->chg_type == DWC3_DCP_CHARGER || dotg->charger->chg_type == DWC3_PROPRIETARY_CHARGER) && ac_psy) {
 		pr_info("%s: override dotg->psy to ac->psy\n", __func__);
 		saved_usb_psy = dotg->psy;
 		dotg->psy = ac_psy;
@@ -749,8 +753,17 @@ static void dwc3_otg_sm_work(struct work_struct *w)
 					work = 1;
 					break;
 				case DWC3_SDP_CHARGER:
+#ifdef CONFIG_FORCE_FAST_CHARGE
+					if (fast_charge_level > FAST_CHARGE_0)
+						dwc3_otg_set_power(phy,
+							min(fast_charge_level, DWC3_IDEV_CHG_MAX));
+					else
+						dwc3_otg_set_power(phy,
+							DWC3_IDEV_CHG_MIN);
+#else
 					dwc3_otg_set_power(phy,
 							DWC3_IDEV_CHG_MIN);
+#endif
 					if (!slimport_is_connected()) {
 						dwc3_otg_start_peripheral(
 								&dotg->otg,
