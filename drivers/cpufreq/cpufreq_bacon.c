ed1eae347f40e9cef416452892ebfa70e2315488
Saravana Kannan <skannan@codeaurora.org>
2014-10-15 12:44:18 -0700

    cpufreq: bacon: Exercise hispeed settings at a policy level
    If a heavy task migrates between otherwise idle CPUs in a policy during
    every sample window, the above hispeed delay window for the CPUs would get
    restarted for every sample window. Due to the continuous restart of above
    hispeed delay window, none of the CPUs would ever pick a target frequency
    higher than hispeed frequency. This causes the policy's frequency to be
    stuck at hispeed freq even if the load justifies a higher frequency.
    
    To fix this, the above high speed delay window is restarted only when the
    policy frequency changes. This ensures that tasks migrating between CPUs in
    a policy are handled correctly.
    
    Also, the hispeed load/frequency heuristic is only necessary when the
    information is insufficient to determine if the load on the CPU needs at
    least hispeed frequency. When the policy frequency is already at or above
    hispeed frequency, if the CPU load% based on policy frequency is not above
    hispeed load, then the information is clearly sufficient to determine that
    the load on the CPU does not need hispeed frequency.
    
    Therefore, compute CPU load% (which is used only to compare against hispeed
    load) based on policy frequency instead of CPU target frequency.
    
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpufreq_bacon.c
+++ drivers/cpufreq/cpufreq_bacon.c
@@ -382,11 +382,11 @@ static void cpufreq_interactive_timer(unsigned long data)
 	spin_lock_irqsave(&pcpu->target_freq_lock, flags);
 	do_div(cputime_speedadj, delta_time);
 	loadadjfreq = (unsigned int)cputime_speedadj * 100;
-	cpu_load = loadadjfreq / pcpu->target_freq;
+	cpu_load = loadadjfreq / pcpu->policy->cur;
 	boosted = boost_val || now < boostpulse_endtime;
 
 	if (cpu_load >= go_hispeed_load || boosted) {
-		if (pcpu->target_freq < hispeed_freq) {
+		if (pcpu->policy->cur < hispeed_freq) {
 			new_freq = hispeed_freq;
 		} else {
 			new_freq = choose_freq(pcpu, loadadjfreq);
@@ -398,15 +398,13 @@ static void cpufreq_interactive_timer(unsigned long data)
 		new_freq = choose_freq(pcpu, loadadjfreq);
 	}
 
-	if (pcpu->target_freq >= hispeed_freq &&
-	    new_freq > pcpu->target_freq &&
+	if (pcpu->policy->cur >= hispeed_freq &&
+	    new_freq > pcpu->policy->cur &&
 	    now - pcpu->hispeed_validate_time <
-	    freq_to_above_hispeed_delay(pcpu->target_freq)) {
+	    freq_to_above_hispeed_delay(pcpu->policy->cur)) {
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto rearm;
 	}
-
-	pcpu->hispeed_validate_time = now;
 
 	if (cpufreq_frequency_table_target(pcpu->policy, pcpu->freq_table,
 					   new_freq, CPUFREQ_RELATION_L,
@@ -580,6 +578,7 @@ static int cpufreq_interactive_speedchange_task(void *data)
 		for_each_cpu(cpu, &tmp_mask) {
 			unsigned int j;
 			unsigned int max_freq = 0;
+			struct cpufreq_interactive_cpuinfo *pjcpu;
 
 			pcpu = &per_cpu(cpuinfo, cpu);
 			if (!down_read_trylock(&pcpu->enable_sem))
@@ -590,17 +589,23 @@ static int cpufreq_interactive_speedchange_task(void *data)
 			}
 
 			for_each_cpu(j, pcpu->policy->cpus) {
-				struct cpufreq_interactive_cpuinfo *pjcpu =
-					&per_cpu(cpuinfo, j);
+				pjcpu = &per_cpu(cpuinfo, j);
 
 				if (pjcpu->target_freq > max_freq)
 					max_freq = pjcpu->target_freq;
 			}
 
-			if (max_freq != pcpu->policy->cur)
+			if (max_freq != pcpu->policy->cur) {
+				u64 now;
 				__cpufreq_driver_target(pcpu->policy,
 							max_freq,
 							CPUFREQ_RELATION_H);
+				now = ktime_to_us(ktime_get());
+				for_each_cpu(j, pcpu->policy->cpus) {
+					pjcpu = &per_cpu(cpuinfo, j);
+					pjcpu->hispeed_validate_time = now;
+				}
+			}
 
 			up_read(&pcpu->enable_sem);
 		}
