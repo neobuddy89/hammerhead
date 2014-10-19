7ff7600561a0ef034d6db16ef03a48758508dcb8
Pranav Vashi <neobuddy89@gmail.com>
2014-10-19 10:04:47 +0530

    build: Use 4.9.2 hammerhead optimized toolchain
    * This toolchain is compiled from GCC Sabermod sources
      and optimized to use with hammerhead.
    * Linaro 4.9.2 seems to affect battery life while this
      one is easy on battery and better on performance.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- env_setup.sh
+++ env_setup.sh
@@ -65,7 +65,7 @@ export KBUILD_BUILD_HOST="DragonCore";
 # export CROSS_COMPILE=$PARENT_DIR/../toolchains/arm-eabi-4.7/bin/arm-eabi-;
 
 # Use hammerhead optimized toolchain!
-export CROSS_COMPILE=$PARENT_DIR/../toolchains/arm-hammerhead-linux-gnueabi-4.8.4/bin/arm-hammerhead-linux-gnueabi-;
+export CROSS_COMPILE=$PARENT_DIR/../toolchains/arm-hammerhead-linux-gnueabi-4.9.2/bin/arm-hammerhead-linux-gnueabi-;
 
 if [ ! -f ${CROSS_COMPILE}gcc ]; then
 	echo "${bldred}Cannot find GCC compiler ${CROSS_COMPILE}gcc${txtrst}";
