c53a1ce4f3d5baabeea9f29e1d42b6c0e649bf8f
Pranav Vashi <neobuddy89@gmail.com>
2015-01-03 17:13:32 +0530

    Revert "msm: mdss: Unbreak the build"
    * Next patch fixes this properly.
    
    This reverts commit 4f4f9b9578837f374cea75a90b6e1b924b699989.

--- drivers/video/msm/mdss/mdss_mdp_trace.h
+++ drivers/video/msm/mdss/mdss_mdp_trace.h
@@ -252,4 +252,4 @@ TRACE_EVENT(mdp_trace_counter,
 #endif /* if !defined(TRACE_MDSS_MDP_H) || defined(TRACE_HEADER_MULTI_READ) */
 
 /* This part must be outside protection */
-//#include <trace/define_trace.h>
+#include <trace/define_trace.h>
