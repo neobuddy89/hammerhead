36d927b0ce520c3802becb4eaf5fc0fc45b11bd5
Pranav Vashi <neobuddy89@gmail.com>
2015-01-09 11:59:05 +0530

    mm: ksm: Enable deferred timer by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/ksm.c
+++ mm/ksm.c
@@ -189,7 +189,7 @@ static unsigned int ksm_thread_pages_to_scan = 256;
 static unsigned int ksm_thread_sleep_millisecs = 1500;
 
 /* Boolean to indicate whether to use deferred timer or not */
-static bool use_deferred_timer;
+static bool use_deferred_timer = true;
 
 #define KSM_RUN_STOP	0
 #define KSM_RUN_MERGE	1
