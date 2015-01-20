31f2130d29d223d7291b6ca9f5d00b7701aaadc0
Pranav Vashi <neobuddy89@gmail.com>
2015-01-20 23:09:56 +0530

    cpufreq: Add smartmax governor
    * On mass demand.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -391,6 +391,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
