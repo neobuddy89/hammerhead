2c1a9391e9b306259b40a6d9365887377241c301
Pranav Vashi <neobuddy89@gmail.com>
2015-02-01 16:35:04 +0530

    build: Boot with SELinux Permissive
    * We need device to be in permissive selinux so that
      scripts from ram disk execute properly before setting
      selinux to enforcing mode.
    * All hail CM12 for pissing selinux policies.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -109,7 +109,7 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage-dtb ]; then
 	
 	echo "--- Creating boot.img ---"
 	# copy all needed to out kernel folder
-	./utilities/mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1 androidboot.bootdevice=msm_sdcc.1' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output boot.img
+	./utilities/mkbootimg --kernel zImage-dtb --cmdline 'console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive' --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02900000 --tags_offset 0x02700000 --ramdisk ramdisk.gz --output boot.img
 
 	rm $KERNELDIR/out/boot.img >> /dev/null;
 	rm $KERNELDIR/out/Hybrid_* >> /dev/null;
