arter97/caf_msm-5.4

msm: mdss: add support for display always on mode
Add a new panel power state to represent a panel low power mode. Add a
new sysfs node to control when to configure the panel into this new
mode.

Change-Id: I7df8bf63470bec893c6b7481678f00ce76b6875c
Signed-off-by: Aravind Venkateswaran <aravindh@codeaurora.org>

drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -3047,7 +3047,13 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		}
 	}
 
-	atomic_inc(&mdp5_data->mdata->ov_active_panels);
+	if (mfd->panel_power_state == MDSS_PANEL_POWER_OFF) {
+		atomic_inc(&mdp5_data->mdata->ov_active_panels);
+	} else {
+		pr_debug("panel was not off. do not inc active panel ctn\n");
+		rc = mdss_mdp_ctl_start(mdp5_data->ctl, false);
+		goto panel_on;
+	}
 
 	if (!mfd->panel_info->cont_splash_enabled &&
 		(mfd->panel_info->type != DTV_PANEL)) {
@@ -3062,6 +3068,7 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 			goto error_pm;
 	}
 
+panel_on:
 	if (IS_ERR_VALUE(rc)) {
 		pr_err("Failed to turn on fb%d\n", mfd->index);
 		mdss_mdp_overlay_off(mfd);
@@ -3106,6 +3113,11 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 	 */
 	pm_runtime_get_sync(&mfd->pdev->dev);
 
+	if (mfd->panel_power_state != MDSS_PANEL_POWER_OFF) {
+		pr_debug("panel not turned off. keeping overlay on\n");
+		goto ctl_stop;
+	}
+
 	mutex_lock(&mdp5_data->ov_lock);
 
 	mdss_mdp_overlay_free_fb_pipe(mfd);
@@ -3142,24 +3154,27 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 		__vsync_retire_signal(mfd, mdp5_data->retire_cnt);
 	}
 
+ctl_stop:
 	mutex_lock(&mdp5_data->ov_lock);
 	rc = mdss_mdp_ctl_stop(mdp5_data->ctl, mfd->panel_power_state);
 	if (rc == 0) {
-		mutex_lock(&mdp5_data->list_lock);
-		__mdss_mdp_overlay_free_list_purge(mfd);
-		mutex_unlock(&mdp5_data->list_lock);
-		mdss_mdp_ctl_notifier_unregister(mdp5_data->ctl,
-				&mfd->mdp_sync_pt_data.notifier);
+		if (mfd->panel_power_state == MDSS_PANEL_POWER_OFF) {
+			mutex_lock(&mdp5_data->list_lock);
+			__mdss_mdp_overlay_free_list_purge(mfd);
+			mutex_unlock(&mdp5_data->list_lock);
+			mdss_mdp_ctl_notifier_unregister(mdp5_data->ctl,
+					&mfd->mdp_sync_pt_data.notifier);
 
-		if (!mfd->ref_cnt) {
-			mdp5_data->borderfill_enable = false;
-			mdss_mdp_ctl_destroy(mdp5_data->ctl);
-			mdp5_data->ctl = NULL;
+			if (!mfd->ref_cnt) {
+				mdp5_data->borderfill_enable = false;
+				mdss_mdp_ctl_destroy(mdp5_data->ctl);
+				mdp5_data->ctl = NULL;
+			}
+
+			if (atomic_dec_return(
+				&mdp5_data->mdata->ov_active_panels) == 0)
+				mdss_mdp_rotator_release_all();
 		}
-
-		if (atomic_dec_return(
-			&mdp5_data->mdata->ov_active_panels) == 0)
-			mdss_mdp_rotator_release_all();
 
 		if (!mdp5_data->mdata->idle_pc_enabled) {
 			rc = pm_runtime_put(&mfd->pdev->dev);

neobuddy89/hammerhead

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2895,6 +2895,12 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		mdp5_data->ctl = ctl;
 	}
 
+	if (mdss_fb_is_panel_power_on(mfd)) {
+		pr_debug("panel was never turned off\n");
+		rc = mdss_mdp_ctl_start(mdp5_data->ctl, false);
+		goto panel_on;
+	}
+
 	if (!mfd->panel_info->cont_splash_enabled &&
 		(mfd->panel_info->type != DTV_PANEL)) {
 		rc = mdss_mdp_overlay_start(mfd);
@@ -2910,6 +2916,7 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 			goto end;
 	}
 
+panel_on:
 	if (IS_ERR_VALUE(rc)) {
 		pr_err("Failed to turn on fb%d\n", mfd->index);
 		mdss_mdp_overlay_off(mfd);
@@ -2951,6 +2958,11 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 	 */
 	pm_runtime_get_sync(&mfd->pdev->dev);
 
+	if (mfd->panel_power_state != MDSS_PANEL_POWER_OFF) {
+		pr_debug("panel not turned off. keeping overlay on\n");
+		goto ctl_stop;
+	}
+
 	mdss_mdp_overlay_free_fb_pipe(mfd);
 
 	mixer = mdss_mdp_mixer_get(mdp5_data->ctl, MDSS_MDP_MIXER_MUX_LEFT);
@@ -2984,26 +2996,32 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 		__vsync_retire_signal(mfd, mdp5_data->retire_cnt);
 	}
 
+ctl_stop:
 	rc = mdss_mdp_ctl_stop(mdp5_data->ctl, mfd->panel_power_state);
 	if (rc == 0) {
-		__mdss_mdp_overlay_free_list_purge(mfd);
-		mdss_mdp_ctl_notifier_unregister(mdp5_data->ctl,
-				&mfd->mdp_sync_pt_data.notifier);
+		if (mfd->panel_power_state == MDSS_PANEL_POWER_OFF) {
+			mutex_lock(&mdp5_data->list_lock);
+			__mdss_mdp_overlay_free_list_purge(mfd);
+			mutex_unlock(&mdp5_data->list_lock);
+			mdss_mdp_ctl_notifier_unregister(mdp5_data->ctl,
+					&mfd->mdp_sync_pt_data.notifier);
 
-		if (!mfd->ref_cnt) {
-			mdp5_data->borderfill_enable = false;
-			mdss_mdp_ctl_destroy(mdp5_data->ctl);
-			mdp5_data->ctl = NULL;
-		}
+			if (!mfd->ref_cnt) {
+				mdp5_data->borderfill_enable = false;
+				mdss_mdp_ctl_destroy(mdp5_data->ctl);
+				mdp5_data->ctl = NULL;
+			}
 
-		if (atomic_dec_return(&mdp5_data->mdata->active_intf_cnt) == 0)
-			mdss_mdp_rotator_release_all();
+			if (atomic_dec_return(
+				&mdp5_data->mdata->active_intf_cnt) == 0)
+				mdss_mdp_rotator_release_all();
 
-		if (!mdp5_data->mdata->idle_pc_enabled) {
-			rc = pm_runtime_put_sync(&mfd->pdev->dev);
-			if (rc)
-				pr_err("unable to suspend w/pm_runtime_put (%d)\n",
-					rc);
+			if (!mdp5_data->mdata->idle_pc_enabled) {
+				rc = pm_runtime_put(&mfd->pdev->dev);
+				if (rc)
+					pr_err("unable to suspend w/pm_runtime_put (%d)\n",
+						rc);
+			}
 		}
 	}
 